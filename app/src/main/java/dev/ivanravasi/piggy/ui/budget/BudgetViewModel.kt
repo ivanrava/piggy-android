3587d0f7df671083b87ace29703421b73e360f43
Ivan Ravasi <ivan4.ravasi@gmail.com>
2024-05-20 08:11:54 +0200

    Adds remaining fragments

---
+++ app/src/main/java/dev/ivanravasi/piggy/ui/budget/BudgetViewModel.kt
@@ -0,0 +1,7 @@
+package dev.ivanravasi.piggy.ui.budget
+
+import androidx.lifecycle.ViewModel
+
+class BudgetViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/dev/ivanravasi/piggy/ui/properties/PropertiesViewModel.kt
@@ -0,0 +1,7 @@
+package dev.ivanravasi.piggy.ui.properties
+
+import androidx.lifecycle.ViewModel
+
+class PropertiesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
