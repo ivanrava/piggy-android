afc923806a10d0cff65e51ecf735ac8ea8c2d980
Ivan Ravasi <ivan4.ravasi@gmail.com>
2024-05-28 22:08:52 +0200

    Adds property value variation bottom sheets

--- app/src/main/java/dev/ivanravasi/piggy/ui/properties/PropertyAdapter.kt
+++ app/src/main/java/dev/ivanravasi/piggy/ui/properties/PropertyAdapter.kt
@@ -1,12 +1,18 @@
 package dev.ivanravasi.piggy.ui.properties
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.findFragment
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dev.ivanravasi.piggy.R
 import dev.ivanravasi.piggy.api.iconify.loadIconify
 import dev.ivanravasi.piggy.api.piggy.bodies.entities.Property
+import dev.ivanravasi.piggy.databinding.BottomSheetPropertyVariationBinding
 import dev.ivanravasi.piggy.databinding.ListItemPropertyBinding
 import dev.ivanravasi.piggy.ui.setCurrency
 
@@ -28,6 +34,15 @@ class PropertyAdapter: ListAdapter<Property, PropertyAdapter.PropertyViewHolder>
             binding.propertyName.text = property.name
             binding.propertyDescription.text = property.description
             binding.propertyValue.setCurrency(property.value)
+
+            binding.btnPlus.setOnClickListener {
+                val variationBottomSheet = VariationBottomSheet(R.string.title_increment)
+                variationBottomSheet.show(binding.root.findFragment<PropertiesFragment>().parentFragmentManager, "IncrementBottomSheet")
+            }
+            binding.btnMinus.setOnClickListener {
+                val variationBottomSheet = VariationBottomSheet(R.string.title_decrement)
+                variationBottomSheet.show(binding.root.findFragment<PropertiesFragment>().parentFragmentManager, "DecrementBottomSheet")
+            }
         }
 
         companion object {
@@ -35,6 +50,18 @@ class PropertyAdapter: ListAdapter<Property, PropertyAdapter.PropertyViewHolder>
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ListItemPropertyBinding.inflate(layoutInflater, parent, false)
                 return PropertyViewHolder(binding)
+            }
+        }
+
+        class VariationBottomSheet(val titleResource: Int) : BottomSheetDialogFragment() {
+            override fun onCreateView(
+                inflater: LayoutInflater,
+                container: ViewGroup?,
+                savedInstanceState: Bundle?
+            ): View {
+                val binding = BottomSheetPropertyVariationBinding.inflate(inflater, container, false)
+                binding.variationTitle.text = getString(titleResource)
+                return binding.root
             }
         }
     }
