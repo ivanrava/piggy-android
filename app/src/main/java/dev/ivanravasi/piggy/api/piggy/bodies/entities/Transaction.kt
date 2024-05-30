c71a26d8c3a4c8b6fa53c0cc94cd33d01fb96e5b
Ivan Ravasi <ivan4.ravasi@gmail.com>
2024-05-30 08:22:35 +0200

    Adds operation models

---
+++ app/src/main/java/dev/ivanravasi/piggy/api/piggy/bodies/entities/Transaction.kt
@@ -0,0 +1,22 @@
+package dev.ivanravasi.piggy.api.piggy.bodies.entities
+
+import com.google.gson.annotations.SerializedName
+
+data class Transaction(
+    @SerializedName("id")
+    val id: Long,
+    @SerializedName("date")
+    val date: String,
+    @SerializedName("amount")
+    val amount: String,
+    @SerializedName("notes")
+    val notes: String?,
+    @SerializedName("checked")
+    val checked: Boolean,
+    @SerializedName("category")
+    val category: Category,
+    @SerializedName("beneficiary")
+    val beneficiary: Beneficiary,
+    @SerializedName("account")
+    val account: Account,
+)
---
+++ app/src/main/java/dev/ivanravasi/piggy/api/piggy/bodies/entities/Transfer.kt
@@ -0,0 +1,20 @@
+package dev.ivanravasi.piggy.api.piggy.bodies.entities
+
+import com.google.gson.annotations.SerializedName
+
+data class Transfer(
+    @SerializedName("id")
+    val id: Long,
+    @SerializedName("date")
+    val date: String,
+    @SerializedName("amount")
+    val amount: String,
+    @SerializedName("notes")
+    val notes: String?,
+    @SerializedName("checked")
+    val checked: Boolean,
+    @SerializedName("to")
+    val to: Account?,
+    @SerializedName("from")
+    val from: Account?,
+)
